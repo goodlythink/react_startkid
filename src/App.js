fb9789373de6cfb883f632e2955e9d046bbd5f1f
GoodlyThink <goodlythink@gmail.com>
2017-10-03 11:27:02 +0700

    update src

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ChildrenWithProps from './ChildrenWithProps'
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <ChildrenWithProps />
+            </div>
+        )
+    }
+}
+
+export default App
